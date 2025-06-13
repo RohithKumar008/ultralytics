bd9ad135b2f72120e42e2a96a833024a1e4048f0
RohithKumar <148546776+RohithKumar008@users.noreply.github.com>
2025-06-13 21:22:20 +0530

    Update loss.py
    Signed-off-by: RohithKumar <148546776+RohithKumar008@users.noreply.github.com>

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -200,7 +200,7 @@ class v8DetectionLoss:
         h = model.args  # hyperparameters
 
         m = model.model[-1]  # Detect() module
-        self.bce = nn.BCEWithLogitsLoss(reduction="none")
+        self.bce = FocalLoss(gamma=2.0, alpha=0.25)
         self.hyp = h
         self.stride = m.stride  # model strides
         self.nc = m.nc  # number of classes
