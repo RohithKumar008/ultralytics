eac98c92514325cdc6ad4de015a50d2670ccb4e7
RohithKumar <148546776+RohithKumar008@users.noreply.github.com>
2025-06-26 06:42:39 +0530

    Update tasks.py
    Signed-off-by: RohithKumar <148546776+RohithKumar008@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -441,7 +441,7 @@ class DetectionModel(BaseModel):
         # Build strides
         m = self.model[-1]  # Detect()
         if isinstance(m, Detect):  # includes all Detect subclasses like Segment, Pose, OBB, YOLOEDetect, YOLOESegment
-            s = 256  # 2x min stride
+            s = 640  # 2x min stride
             m.inplace = self.inplace
 
             def _forward(x):
