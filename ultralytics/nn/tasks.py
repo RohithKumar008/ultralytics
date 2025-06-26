29013eb090dcffe49c8f7634786d62169beeb190
RohithKumar <148546776+RohithKumar008@users.noreply.github.com>
2025-06-26 14:38:06 +0530

    Update tasks.py
    Signed-off-by: RohithKumar <148546776+RohithKumar008@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -56,6 +56,7 @@ from ultralytics.nn.modules import (
     ImagePoolingAttn,
     Index,
     TripletAttention,
+    DynamicRouting,
     CrossAttentionFuse,
     GatedFusion,
     DWSConv,
@@ -90,6 +91,7 @@ from ultralytics.nn.modules import (
     v10Detect,
 )
 
+globals()['DynamicRouting'] = DynamicRouting
 globals()['CrossAttentionFuse'] = CrossAttentionFuse
 globals()['TripletAttention'] = TripletAttention
 globals()['GatedFusion'] = GatedFusion
@@ -1772,6 +1774,8 @@ def parse_model(d, ch, verbose=True):
         elif m is CrossAttentionFuse:
             assert all(ch[x] == ch[f[0]] for x in f), f"All inputs to {m} must have same channels"
             c2 = ch[f[0]]  # or max(ch[x] for x in f) if they differ and you handle it
+        elif m is DynamicRouting:
+            c2 = args[1]  # out_channels
         elif m in frozenset(
             {Detect, WorldDetect, YOLOEDetect, Segment, YOLOESegment, Pose, OBB, ImagePoolingAttn, v10Detect}
         ):
