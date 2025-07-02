845c35378b5f268483b0f04c8acc0fcd17359768
RohithKumar <148546776+RohithKumar008@users.noreply.github.com>
2025-07-02 21:48:18 +0530

    Update tasks.py
    Signed-off-by: RohithKumar <148546776+RohithKumar008@users.noreply.github.com>

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1722,7 +1722,6 @@ def parse_model(d, ch, verbose=True):
             C2fCIB,
             C2PSA,
             A2C2f,
-            AddBlock,
         }
     )
     for i, (f, n, m, args) in enumerate(d["backbone"] + d["head"]):  # from, number, module, args
